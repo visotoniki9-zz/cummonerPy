1738e7f2352d16dff49b6960392fc2be536ccf82
visotoniki9 <d.simon.ssx0@gmail.com>
2021-11-09 01:16:23 +0100

    Finished

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# CummonerPy
+This is a simple script to download all the chapters from [The Cummoner](https://totempole666.com/) adult webcomic from its official website, all neatly organized per chapters.  
+Just run:
+```
+python cummoner.py
+```
+It will create a downloads folder and a folder for each chapter like, I. First Time For Everything, now don't expect this thing to be fast, i did this as an exercise for learning scraping in python. It does use async but the code could be improved ill probably do it later... maybe. Anyway it takes about 2min to finish.
---
+++ cummoner.py
@@ -0,0 +1,66 @@
+import os
+import time
+import aiohttp
+import aiofiles
+import asyncio
+import mimetypes
+from bs4 import BeautifulSoup
+
+start_time = time.time()
+
+
+async def downloadImage(session, image_url, path):
+    async with session.get(image_url) as response:
+        assert response.status == 200
+        extension = mimetypes.guess_extension(response.headers['content-type'])
+        image = await aiofiles.open(path+extension, mode='wb')
+        await image.write(await response.read())
+        await image.close()
+        print(f'Finished:{path}')
+
+
+async def getImageUrl(session, page_url):
+    async with session.get(page_url) as page_res:
+        page_soup = BeautifulSoup(await page_res.text(), 'html.parser')
+        image_url = page_soup.find('img')['src']
+        return image_url
+
+
+async def main():
+
+    async with aiohttp.ClientSession() as session:
+        # Get html body of main page and pass is as respose
+        async with session.get('https://totempole666.com/archives-2') as response:
+            # Mount the html on parser
+            soup = BeautifulSoup(await response.text(), 'html.parser')
+            # Get all chapter containers
+            chapters = soup.select('.comic-archive-chapter-wrap')
+
+            for chapter in chapters:
+                chapter_title = chapter.select_one(
+                    '.comic-archive-chapter').get_text()
+
+                pages = chapter.select('.comic-archive-title')
+                pages_url_tasks = []
+
+                for page in pages:
+                    # Get page url
+                    page_url = page.find('a', href=True)['href']
+                    # Append page url to tasks list
+                    pages_url_tasks.append(asyncio.ensure_future(
+                        getImageUrl(session, page_url)))
+
+                image_urls = await asyncio.gather(*pages_url_tasks)
+
+                download_images_task = []
+                for index, image_url in enumerate(image_urls):
+                    if not os.path.exists(f'downloads/{chapter_title}/'):
+                        os.makedirs(f'downloads/{chapter_title}/')
+                    path = f'downloads/{chapter_title}/page{index+1}'
+                    download_images_task.append(asyncio.ensure_future(
+                        downloadImage(session, image_url, path)))
+
+                await asyncio.gather(*download_images_task)
+
+asyncio.run(main())
+print(time.time()-start_time)
